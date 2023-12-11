32a22b3404fd0f8854bfe78bc42e49dd79bbc645
BrklPprk <149465074+BrklPprk@users.noreply.github.com>
2023-12-11 14:03:55 +0900

    config status innerText change

--- js/config.js
+++ js/config.js
@@ -16,7 +16,13 @@ configIcon.addEventListener("click", openConfig);
 
 // config switch toggle
 const switchBgnAni = document.querySelector("#switch__bgn-animation");
+const switchBgnAniTxt = document.querySelector(
+  ".config__list--components:nth-child(1) .config__list--status"
+);
 const switchRqstLoc = document.querySelector("#switch__req-location");
+const switchRqstLocTxt = document.querySelector(
+  ".config__list--components:nth-child(2) .config__list--status"
+);
 const centerElements = document.querySelector(".center-elements");
 const config = document.querySelector(".config");
 
@@ -26,15 +32,20 @@ const RQST_LOC_KEY = "requestLocation";
 function setBgnAniStatus() {
   if (switchBgnAni.checked === true) {
     localStorage.setItem(BGN_ANI_KEY, true);
+    switchBgnAniTxt.innerText = "Enable";
   } else {
     localStorage.setItem(BGN_ANI_KEY, false);
+    switchBgnAniTxt.innerText = "Disable";
   }
 }
+
 function setRqstLocStatus() {
   if (switchRqstLoc.checked === true) {
     localStorage.setItem(RQST_LOC_KEY, true);
+    switchRqstLocTxt.innerText = "Enable";
   } else {
     localStorage.setItem(RQST_LOC_KEY, false);
+    switchRqstLocTxt.innerText = "Disable";
   }
 }
 
@@ -43,6 +54,7 @@ switchRqstLoc.addEventListener("change", setRqstLocStatus);
 
 if (localStorage.getItem(BGN_ANI_KEY) === "false") {
   switchBgnAni.checked = false;
+  switchBgnAniTxt.innerText = "Disable";
   bgImage.classList.remove("fadeout");
   centerElements.classList.remove("welcome-animation");
   config.classList.remove("welcome-animation");
@@ -50,6 +62,7 @@ if (localStorage.getItem(BGN_ANI_KEY) === "false") {
 
 if (localStorage.getItem(RQST_LOC_KEY) === "false") {
   switchRqstLoc.checked = false;
+  switchRqstLocTxt.innerText = "Disable";
   onGeoErr();
 } else {
   navigator.geolocation.getCurrentPosition(onGeoOk, onGeoErr);
